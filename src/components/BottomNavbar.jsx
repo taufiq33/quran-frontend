967a8c1337d21698d0b9c198489056d0b48a296a
Taufiq33 <taufiqhidayat1908@gmail.com>
2025-06-08 18:52:38 +0700

    feat: fix style icon and route

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -2,23 +2,35 @@ import QuranMenuIcon from "../assets/quran-menu-icon.svg";
 import SholatIcon from "../assets/sholat-icon.svg";
 import BookmarkIcon from "../assets/bookmark-icon.svg";
 import SettingsIcon from "../assets/settings_icon.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuran } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
 
 export default function BottomNavbar() {
   return (
     <>
-      <div className="header fixed w-full bottom-0 z-100 bg-stone-50 shadow-lg flex justify-evenly items-center gap-1 px-4 py-2 border-t-stone-100 border-t-1">
+      <div className="header fixed w-full bottom-0 z-100 bg-stone-50 shadow-lg flex justify-around items-center p-2 border-t-stone-100 border-t-1">
         <NavLink
-          className={({ isActive }) => (isActive ? "active" : null)}
-          to={`/surah`}
+          className={({ isActive }) =>
+            isActive ? "active" : "grey-custom-filter"
+          }
+          to={`/list-surah`}
         >
-          <img className="scale-80" src={QuranMenuIcon} alt="" />
+          <FontAwesomeIcon icon={faQuran} className="scale-130 mr-2" />
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "active" : null)}
           to={`/sholat`}
         >
           <img className="scale-80" src={SholatIcon} alt="" />
+        </NavLink>
+        <NavLink
+          className={() =>
+            location.pathname.startsWith("/surah/") ? "active" : null
+          }
+          to="/surah/1"
+        >
+          <img className="scale-80" src={QuranMenuIcon} alt="" />
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "active" : null)}
