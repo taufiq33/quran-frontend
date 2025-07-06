f78e4190463131ab5a61907b951994e0a8a4d6cd
Taufiq33 <taufiqhidayat1908@gmail.com>
2025-07-06 11:44:24 +0700

    feat: enhance Ayah component with customizable font sizes and conditional rendering for translation

--- src/components/Ayah.jsx
+++ src/components/Ayah.jsx
@@ -1,17 +1,30 @@
-export default function Ayah({ ayah }) {
+import { ARABIC_FONT_SIZE } from "../constant/ARABIC_FONT_SIZE";
+
+export default function Ayah({
+  ayah,
+  arabicFontSize = "normal",
+  showTranslation = true,
+  showTransliteration = true,
+}) {
   return (
     <div className="ayah-section flex flex-col gap-8">
-      <div className="arabic-ayah arabic flex text-3xl text-right justify-end">
-        <span>{ayah.teksArab}</span>
+      <div className={`arabic-ayah arabic flex text-right justify-end `}>
+        <span className={`${ARABIC_FONT_SIZE[arabicFontSize]}`}>
+          {ayah.teksArab}
+        </span>
       </div>
 
-      <div className="translation-ayah italic text-sm text-stone-800">
-        {ayah.teksLatin}
-      </div>
+      {showTransliteration && (
+        <div className="translation-ayah italic text-sm text-stone-800">
+          {ayah.teksLatin}
+        </div>
+      )}
 
-      <div className="translation-ayah text-sm text-stone-500">
-        {ayah.teksIndonesia}
-      </div>
+      {showTranslation && (
+        <div className="translation-ayah text-sm text-stone-500">
+          {ayah.teksIndonesia}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/constant/ARABIC_FONT_SIZE.js
@@ -0,0 +1,7 @@
+export const ARABIC_FONT_SIZE = {
+  "xtra-kecil": "text-xl",
+  kecil: "text-2xl",
+  normal: "text-3xl",
+  besar: "text-4xl",
+  "xtra-besar": "text-5xl",
+};
