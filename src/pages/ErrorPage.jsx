c2fb8539d67dc2ab0ff77ef99934461fcc2a109c
Taufiq33 <taufiqhidayat1908@gmail.com>
2025-08-12 05:55:09 +0700

    feat(errorPage) : add button to refresh app / back to ListSurahPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import BottomNavbar from "../components/BottomNavbar";
 import Header from "../components/Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -21,17 +21,33 @@ export default function ErrorPage() {
           style={{ color: "#B197FC" }}
           className="text-6xl mb-4"
         />
-        <p className="text-lg mb-4">
-          {isNotFound
-            ? "Halaman yg anda minta tidak ditemukan."
-            : "Terjadi Kesalahan, silahkan cek koneksi internet Anda."}
-        </p>
-        {errorObject.statusText && (
-          <p className="text-red-500">{errorObject.statusText}</p>
-        )}
-        {errorObject.message && (
-          <p className="text-red-500">{errorObject.message}</p>
-        )}
+        <div>
+          <p className="text-lg mb-4">
+            {isNotFound
+              ? "Halaman yg anda minta tidak ditemukan."
+              : "Terjadi Kesalahan, silahkan cek koneksi internet Anda."}
+          </p>
+          {errorObject.statusText && (
+            <p className="text-red-500">{errorObject.statusText}</p>
+          )}
+          {errorObject.message && (
+            <p className="text-red-500">{errorObject.message}</p>
+          )}
+          <div className="mt-4 flex flex-col gap-2 items-center justify-center">
+            <button
+              onClick={() => window.location.reload()}
+              className="p-2 cursor-pointer hover:bg-purple-500 bg-purple-800 text-white rounded shadow-xl border-1 border-purple-800"
+            >
+              Refresh
+            </button>
+            <Link
+              to="/list-surah"
+              className="p-2 cursor-pointer text-purple-900 font-bold rounded underline "
+            >
+              Kembali ke daftar surat.
+            </Link>
+          </div>
+        </div>
       </div>
       <BottomNavbar />
     </>
